1d5681c7fbe7711067573442f1efc3c0ebe0d988
broccolism <mile3880@gmail.com>
2021-07-03 12:47:10 +0900

    :sparkles: init commit

--- src/ibcf.py
+++ src/ibcf.py
@@ -101,25 +101,27 @@ def err_rmse(test_matrix, pred_matrix):
 
 
 def get_recommendations(pred, user_id):
+    print(np.argsort(pred[user_id - 1]))
     descending_indicies = np.argsort(
         pred[user_id - 1])[(-1 * N_RECOMMENDATIONS):]
-    train_data = pd.read_table(DATA_PATH + MOVIES_PATH,
+    print(descending_indicies)
+    movie_data = pd.read_table(DATA_PATH + MOVIES_PATH,
                                sep=SEPERATOR, header=None, names=MOVIES_COLUMNS)
 
-    recommendations = [train_data.iloc[i] for i in descending_indicies]
+    # recommendations = [movie_data.iloc[i + 1] for i in descending_indicies]
+    recommendations = movie_data.iloc[descending_indicies + 1]
     return recommendations
 
 
-if __name__ == "__main__":
-    train, test = init_data()
-    print(f'done init data')
-    sim = similarities(train)
-    print(f'done sim')
-    pred = np.copy(predictions(train, sim))
-    print(f'done pred')
-    print(pred)
-    err = err_rmse(test, pred)
-    print(f'done training. RMSE = ', err)
+train, test = init_data()
+print(f'done init data')
+sim = similarities(train)
+print(f'done sim')
 
-    target_user = int(input("user id for recommendation: "))
-    print(get_recommendations(pred, target_user))
+pred = np.copy(predictions(train, sim))
+print(f'done pred')
+err = err_rmse(test, pred)
+print(f'done training. RMSE = ', err)
+
+target_user = int(input("user id for recommendation: ")) - 1
+print(get_recommendations(pred, target_user))
