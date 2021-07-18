5e323e112f9531c5b370a894475b86839c9f49e6
broccolism <mile3880@gmail.com>
2021-07-18 14:39:36 +0900

    :sparkles: server - database test done

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,1 +1,14 @@
-// make and exprot DB connection
+import * as mysql from "mysql";
+
+const config = {
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "0000",
+  database: "movie_service",
+};
+
+const db = mysql.createConnection(config);
+db.connect();
+
+export default db;
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,11 +1,17 @@
 import * as express from "express";
+import db from "../database/connection";
 
 const app: express.Application = express();
 
 app.get(
   "/",
   (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.send("hello typescript express!");
+    return db.query("SELECT * FROM MOVIE WHERE id=100", (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      return res.send(result);
+    });
+    // res.send("hello typescript express!");
   }
 );
 
