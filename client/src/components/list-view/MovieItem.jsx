b1188f454acd9548b77a7a5073ece6093419a34f
brislun <brislun030@gmail.com>
2021-07-27 13:18:42 +0900

    feat : Modify star rating css

--- client/src/components/list-view/MovieItem.jsx
+++ client/src/components/list-view/MovieItem.jsx
@@ -1,9 +1,20 @@
 import react, { useState, useEffect, useRef } from "react";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
 import Rating from "@material-ui/lab/Rating";
+import { withStyles } from "@material-ui/core/styles";
 
 import poster from "../../images/lalaland.jpg";
 import "../../styles/MovieItem.scss";
+
+const StyledRating = withStyles({
+  iconFilled: {
+    color: "white",
+  },
+  iconEmpty: {
+    stroke: "white",
+    strokeWidth: "1px",
+  },
+})(Rating);
 
 function MovieItem() {
   const [star, setStar] = useState(5);
@@ -14,24 +25,24 @@ function MovieItem() {
     setIsSelected((prev) => !prev);
   }
 
-  useEffect(() => {
-    function handleClickOutside(e) {
-      if (starRef.current && !starRef.current.contains(e.target)) {
-        onClickPoster();
-      }
-    }
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [starRef]);
+  // useEffect(() => {
+  //   function handleClickOutside(e) {
+  //     if (starRef.current && !starRef.current.contains(e.target)) {
+  //       onClickPoster();
+  //     }
+  //   }
+  //   document.addEventListener("mousedown", handleClickOutside);
+  //   return () => {
+  //     document.removeEventListener("mousedown", handleClickOutside);
+  //   };
+  // }, [starRef]);
 
   return (
     <div className="movie-item">
       <img src={poster} alt="movie" onClick={onClickPoster}></img>
       {isSelected ? (
-        <div className="movie-item__selected" onClick={onClickPoster}>
-          <Rating
+        <div className="movie-item__selected">
+          <StyledRating
             ref={starRef}
             className="movie-item__star"
             name="simple-controlled"
