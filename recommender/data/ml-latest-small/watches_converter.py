56ab90aba41c0c8276973d17f7170e6b69b7b7f3
broccolism <mile3880@gmail.com>
2021-07-31 13:29:41 +0900

    #17 :sparkles: timestamp - 1 된 watch 테이블 데이터

--- recommender/data/ml-latest-small/watches_converter.py
+++ recommender/data/ml-latest-small/watches_converter.py
@@ -2,13 +2,15 @@
 
 import pandas as pd
 
-# RESULT_COLUMN = ['id', 'user_id', 'movie_id', 'rating', 'timestamp']
+# RESULT_COLUMN = ['id', 'user_id', 'movie_id', 'timestamp']
 RATINGS_COLUMN = ['user_id', 'movie_id', 'rating', 'timestamp']
 
 
 if __name__ == "__main__":
     ratings = pd.read_csv('./ratings.csv', header=None, names=RATINGS_COLUMN)
 
-    ratings.to_csv('./rating_to_database.csv',
+    ratings['timestamp'] = ratings['timestamp'].apply(lambda str: int(str) - 1)
+
+    ratings.to_csv('./watching_to_database.csv',
                    sep=',', na_rep='NaN', index=True)
     print("DONE!")
