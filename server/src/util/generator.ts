c4f3d4a0023c897350d369e3c7ea3a64374a0883
broccolism <mile3880@gmail.com>
2021-08-08 23:41:51 +0900

    #18 :sparkles: movie & user API

--- server/src/util/generator.ts
+++ server/src/util/generator.ts
@@ -1,1 +1,7 @@
+import BaseResponse from "../common/common.model";
+
 export const getCurrentTimestamp = () => Math.floor(Date.now() / 1000);
+
+export const get200ConsoleMessage = (name: String, res: BaseResponse) => `
+    @@@@@@@@@@ [${name}] SUCCEED TO SEND RESPONSE: ${res}
+`;
