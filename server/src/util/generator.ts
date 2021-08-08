6b1772cd3f9afb36df9be94817bb26c0f3dc375c
broccolism <mile3880@gmail.com>
2021-08-08 23:47:45 +0900

    #18 :wrench: change console message format

--- server/src/util/generator.ts
+++ server/src/util/generator.ts
@@ -3,5 +3,5 @@ import BaseResponse from "../common/common.model";
 export const getCurrentTimestamp = () => Math.floor(Date.now() / 1000);
 
 export const get200ConsoleMessage = (name: String, res: BaseResponse) => `
-    @@@@@@@@@@ [${name}] SUCCEED TO SEND RESPONSE: ${res}
+@@@@@@@@@@ [${name}] SUCCEED TO SEND:\n${JSON.stringify(res, null, "\t")}
 `;
